22200a763fae84dd7a7d8d96aaf56a5a334561bc
Stanislav Ovsyankin <cancion2able@gmail.com>
2025-07-12 14:37:13 +0500

    Return a basic OK response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,21 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+    public static void main(String[] args) {
+        try (var serverSocket = new ServerSocket(4221);
+             var clientSocket = serverSocket.accept();
+             var out = clientSocket.getOutputStream()
+        ) {
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            var response = "HTTP/1.1 200 OK\r\n\r\n";
+            out.write(response.getBytes());
+            out.flush();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
