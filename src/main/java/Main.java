3937e6c67938f5c30c355c9a6bd682896b307906
Stanislav Ovsyankin <cancion2able@gmail.com>
2025-07-19 10:38:59 +0500

    Refactor request parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 
 public class Main {
     public static void main(String[] args) {
         try (var serverSocket = new ServerSocket(4221);
              var clientSocket = serverSocket.accept();
-             var in = clientSocket.getInputStream();
-             var out = clientSocket.getOutputStream()
+             var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             var out = new PrintWriter(clientSocket.getOutputStream(), true)
         ) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
 
-            var br = new BufferedReader(new InputStreamReader(in));
-            String line = br.readLine();
+            String responseString, requestString;
 
-            String response = "";
-            if (line != null) {
-                var urlPath = line.split(" ");
-                if (urlPath[1].equals("/")) {
-                    response = "HTTP/1.1 200 OK\r\n\r\n";
-                } else {
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                }
+            requestString = in.readLine();
+            System.out.println("Request string: " + requestString);
+            if (requestString != null && requestString.split(" ")[1].equals("/")) {
+                responseString = "HTTP/1.1 200 OK\r\n\r\n";
+            } else {
+                responseString = "HTTP/1.1 404 Not Found\r\n\r\n";
             }
 
-            out.write(response.getBytes());
-            out.flush();
+            out.println(responseString);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
